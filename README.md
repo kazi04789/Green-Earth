10b18cb8b34387dd4c4efa0afca9ef090db67725
Md.Shanto Kazi <kazishanto058@gmail.com>
2025-09-09 23:56:40 +0600

    some problem code issue and change complete the js all section

---
+++ README.md
@@ -0,0 +1,20 @@
+## 1.- **var**: Function-scoped, can be redeclared.
+- **let**: Block-scoped, can be updated but not redeclared.
+- **const**: Block-scoped, cannot be updated or redeclared.
+
+## 2.
+- **map()** → Creates a new array by transforming each item.
+- **forEach()** → Loops through an array but doesn’t return a new array.
+- **filter()** → Creates a new array with items that meet a condition.
+
+## 3.- Shorter and cleaner way to write functions in JavaScript.
+- Example: Instead of function add(a, b) { return a + b; }, we can write const add = (a, b) => a + b;`.
+
+---
+## 4.- Allows picking values from arrays or objects and storing them in variables easily.
+- Example: const {name, age} = person stores person`’s name and age into separate variables.
+
+---
+## 5.- Strings wrapped in backticks `` ` `` that allow embedding variables with ${}.
+- They are cleaner and easier than using `+` for string concatenation.
+- Example: const greeting = `Hello, ${name}!`` creates a string with the variable included.
--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+// nav-bar toggle
+const menuBtn = document.getElementById("menu-btn");
+const mobileMenu = document.getElementById("mobile-menu");
+
+menuBtn.addEventListener("click", () => {
+  mobileMenu.classList.toggle("hidden");
+});
+
 // category functionaluty
 const loadcategory = () => {
   fetch("https://openapi.programming-hero.com/api/categories")
@@ -37,11 +45,146 @@ const displaycategory = (categories) => {
     }
   });
 };
+// card loadby category................................................................
 
-//--card loadby category data load funtion--//
+/////////////////////// default data load function////////
+
 const loaddefaultData = () => {
   fetch(`https://openapi.programming-hero.com/api/plants`)
     .then((res) => res.json())
     .then((json) => displayCard(json.plants));
 };
 
+// 1.data fetch
+const loadCardbyCategory = (id) => {
+  fetch(`https://openapi.programming-hero.com/api/category/${id}`)
+    .then((res) => res.json())
+    .then((json) => displayCard(json.plants));
+};
+
+// 2.display the card
+const cardcontainer = document.getElementById("card-container");
+
+const displayCard = (plants) => {
+  // console.log(plants);
+  cardcontainer.innerHTML = "";
+  plants.forEach((plant) => {
+    cardcontainer.innerHTML += `<div class=" bg-white shadow-lg md:w-[296px] w-[350px] mx-auto h-[360px] p-3 rounded mb-3">
+    <div >
+            <img src="${plant.image}" alt="" class="h-[180px] w-full" />
+          </div>
+          <h1 class="font-semibold pt-2 " id="${plant.id}">${plant.name}</h1>
+          <p class="text-gray-500 text-[9px] pt-1 pb-1">${plant.description}</p>
+          <div class="flex justify-between pt-1">
+            <h1 class="bg-[#DCFCE7] text-green-700 px-1 rounded text-[12px]">
+              ${plant.category}
+            </h1>
+            <h2 class="font-semibold"><span>$</span>${plant.price}</h2>
+          </div>
+          <button
+            type="button"
+            class=" bg-[#15803D] mt-3 w-full font-bold rounded-xl border-0 shadow-none text-white text-[12px] h-[30px]"
+          >
+            Add to Cart
+          </button>`;
+  });
+};
+
+// load spinner
+const loadspinner = () => {
+  cardcontainer.innerHTML = ` <div
+            class="fixed inset-0 flex flex-col justify-center items-center bg-white/50 z-50"
+          >
+            <div
+              class="w-12 h-12 border-4 border-blue-800 border-t-transparent rounded-full animate-spin"
+            ></div>
+            <p class="mt-4 text-blue-700 font-medium text-sm">Loading...</p>
+          </div>`;
+};
+
+// add to cart functionality
+
+const Addtocartcontainer = document.getElementById("Addtocart-container");
+const plantdetailsmodal = document.getElementById("plant-details-modal");
+const modalcontainer = document.getElementById("modal-container");
+let cartTotal = 0;
+cardcontainer.addEventListener("click", (e) => {
+  // /////////////////////////////////////////////////////// modal
+  if (e.target.tagName === "H1") {
+    // console.log("modal clicked");
+    const plantID = e.target.id;
+    openModal(plantID);
+  }
+
+  if (e.target.innerText === "Add to Cart") {
+    // console.log("cart btn clicked");
+    const plantname = e.target.parentNode.childNodes[3].innerText;
+    const price = e.target.parentNode.childNodes[7].childNodes[3].innerText;
+    const Convertprice = parseFloat(price.replace("$", ""));
+
+    const addtocart = document.createElement("div");
+    addtocart.innerHTML = `<div class="bg-[#F0FDF4] rounded m-2 p-2 h-[65px]">
+            <div class="flex justify-between items-center">
+              <div>
+                <h2 class="text-[14px]">${plantname}</h2>
+                <h3 class="text-[12px] mt-2 text-gray-500"> ${price}</h3>
+              </div>
+              <i class="fa-solid fa-xmark text-gray-400 "></i>
+            </div>
+           
+          </div>
+           `;
+
+    Addtocartcontainer.appendChild(addtocart);
+
+    const totalprice = document.getElementById("total-price");
+    cartTotal += Convertprice;
+    document.getElementById(
+      "total-price"
+    ).innerText = `Total price = ${cartTotal} tk`;
+  }
+});
+// modal open data fetch
+const openModal = (id) => {
+  // plantdetailsmodal.showModal();
+  fetch(`https://openapi.programming-hero.com/api/plant/${id}`)
+    .then((res) => res.json())
+    .then((json) => showdatainModal(json.plants));
+};
+
+const showdatainModal = (plants) => {
+  plantdetailsmodal.showModal();
+
+  modalcontainer.innerHTML = `
+            <h1 class="font-bold pt-2 mb-2 text-[20px]" >${plants.name}</h1> 
+            <img src="${plants.image}" alt="" class="h-[180px] w-full" />
+            <h1 class=" mt-2 mb-2 "> <span class="font-bold text-[13px]">Category:</span>
+            <span class="text-[13px]">${plants.category}</span>
+              
+            </h1>
+            <h2><span class="font-bold text-[13px]">Price:</span> <span class="text-gray-600 text-[13px]">${plants.price}$</span></h2>
+            <p class="text-[10px] pt-2 pb-1"><span class="font-bold text-[13px]">Description:</span><span class="text-[12px]">  ${plants.description}</span></p>`;
+};
+
+// remove div
+Addtocartcontainer.addEventListener("click", (e) => {
+  if (e.target.tagName === "I") {
+    const priceText = e.target.parentNode.childNodes[1].childNodes[3].innerText;
+    // console.log(e.target.parentNode.childNodes[1].childNodes[3].innerText);
+    const price = parseFloat(priceText.replace("$", ""));
+
+    // total
+    cartTotal -= price;
+    const totalPriceEl = document.getElementById("total-price");
+    if (cartTotal > 0) {
+      totalPriceEl.innerText = `Total price = ${cartTotal} tk`;
+    } else {
+      totalPriceEl.innerText = "";
+    }
+
+    // div remove
+    e.target.parentNode.parentNode.remove();
+  }
+});
+loadcategory();
+loaddefaultData();
